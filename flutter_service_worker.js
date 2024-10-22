800d22e3bce12f055e4b5801a0d8c90c188dfcdd
tom1236908745 <ptn-tomoki.nakayama@zozo.com>
2024-10-22 17:45:09 +0900

    fix: `image_pciker_web` を使用

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b0d5eb779dd1c4ecacfb55e3c6ca6e30",
+const RESOURCES = {"flutter_bootstrap.js": "10f6627cc14305451636b62274b720ed",
 "version.json": "0c4dc8caffa903cae6e5e7bdc80c8db6",
 "index.html": "503d558fc7ca26222242ac2ff18651cc",
 "/": "503d558fc7ca26222242ac2ff18651cc",
-"main.dart.js": "a6239920e86eb4e638f4e5d4ed114f5e",
+"main.dart.js": "703e94613932943c8fa3557afb5f3745",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -16,7 +16,7 @@ const RESOURCES = {"flutter_bootstrap.js": "b0d5eb779dd1c4ecacfb55e3c6ca6e30",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "7a8e10ae15aa54f5c8dc35ff1561d6b3",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/NOTICES": "ca7a063cf065b29ee7fe725cd2e69883",
+"assets/NOTICES": "3b4d1a76b195102436af8235ede3fb8c",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
